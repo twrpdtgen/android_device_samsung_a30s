27824933d9d636ca1d2bc87586a1cb61dd2fcf54
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-01 17:01:18 +0200

    a30s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A307GT Script version: 2.2.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a30s.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a30s-user \
+    omni_a30s-userdebug \
+    omni_a30s-eng
